b0652fdd740df858145ffa79e2a73835d71bc413
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2024-12-23 18:15:04 +0700

    change secure properties

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     HttpOnly: true,
-    // secure: true,
+    secure: true,
     sameSite: 'None',
   };
 
