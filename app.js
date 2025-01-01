8ceb3434e9496c27839760b75664653543f2e348
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2025-01-01 22:08:05 +0700

    adding new whitelist to CORS

--- app.js
+++ app.js
@@ -24,7 +24,20 @@ app.set('views', path.join(__dirname, 'views'));
 // 1) GLOBAL MIDDLEWARES
 
 // CORS
-app.use(cors({ credentials: true, origin: 'http://127.0.0.1:3000' }));
+const whitelist = ['https://laporupi.bepramudya.com', 'http://127.0.0.1:3000'];
+
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.options('*', cors());
 
 // Serving static files
